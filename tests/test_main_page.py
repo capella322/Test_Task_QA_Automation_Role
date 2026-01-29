b4a6808b0ae7e75557e9b8c426cd440f5974a3e1
Sukuna <Sukuna@>
2026-01-29 21:46:37 +0300

    init

--- tests/test_main_page.py
+++
@@ -1,8 +0,0 @@
-from pages.main_page import MainPage
-
-def test_landing_content(main_page: MainPage, base_url):
-    main_page.open(base_url)
-    
-    assert main_page.title == "The Internet"
-    assert main_page.fork_me_icon().is_visible()
-    assert main_page.all_links().count() == 46
